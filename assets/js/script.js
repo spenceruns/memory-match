4d6c6859703861e2830b9bd92c92ff560d325fec
Spencer Allen <spenceruns@gmail.com>
2019-11-13 14:41:46 -0800

    Finalized layout with LFZ assets. Was about to make cards clickable but without full fronts (only one the first one in the top left corner

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,16 @@
+$(document).ready(inializeApp);
 
+function inializeApp() {
+  $(".card").on("click", handleCardClick);
+  randomizeCards();
+}
+
+function randomizeCards() {
+  var randomNumber = Math.floor((Math.random() * 8) + 1);;
+
+  var cardFront = ["css-logo", "docker-logo", "gitHub-logo", "html-logo", "js-logo", "mysql-logo", "node-logo", "php-logo", "react-logo"];
+}
+
+function handleCardClick(event) {
+  $(event.currentTarget).find(".back").addClass("hidden");
+}
